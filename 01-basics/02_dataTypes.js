edfaadf9fdebc19bf9e93e291cc74a1b5527edf0
subarna mondal <msubarna878@gmail.com>
2025-02-07 15:11:55 +0000

    dataTypes

--- 01-basics/02_dataTypes.js
+++ 01-basics/02_dataTypes.js
@@ -2,4 +2,22 @@
 
 // alert (3+3) //we are using node.js,not browser
 
+console.log(3+3)
+console.log("subarna")
+
+let name = "neha"
+let age =19
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bright
+// string=>""
+// boolean => standalone value
+// undefined
+// symbol => unique
+
+// object
+console.log(typeof undefined); //undefined
+
 
