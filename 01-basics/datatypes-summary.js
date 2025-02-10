8fe4ebcc4c1b104f176cd013cf62558ae3f992c0
subarna mondal <msubarna878@gmail.com>
2025-02-10 16:12:00 +0000

    container config done

---
+++ 01-basics/datatypes-summary.js
@@ -0,0 +1,20 @@
+// primitive
+
+ // 7 types : string, number,Boolean,null,undefined,symbol,BIgInt
+
+ const score = 100
+ const scoreValue = 100.3
+
+ const isLoggedIn = false
+ const outsideTemp = null
+ let userEmail;
+
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+
+ console.log(id == anotherId);
+
+
+ // Reference (Non primitive)
+
+ // Array,0bjects,Functions
